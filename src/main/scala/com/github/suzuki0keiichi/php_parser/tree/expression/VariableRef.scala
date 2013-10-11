e1d1cd9f1afce567c938529a9a7d0883a54fa7f9
suzuki0keiichi <suzuki@dwango.co.jp>
2013-10-11 19:19:14 +0900

    implement if statement(simple ver)

---
+++ src/main/scala/com/github/suzuki0keiichi/php_parser/tree/expression/VariableRef.scala
@@ -0,0 +1,12 @@
+package com.github.suzuki0keiichi.php_parser.tree.expression
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: suzuki
+ * Date: 13/10/11
+ * Time: 11:26
+ * To change this template use File | Settings | File Templates.
+ */
+class VariableRef {
+
+}
---
+++ src/main/scala/com/github/suzuki0keiichi/php_parser/tree/statement/Line.scala
@@ -0,0 +1,12 @@
+package com.github.suzuki0keiichi.php_parser.tree.statement
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: suzuki
+ * Date: 13/10/11
+ * Time: 13:02
+ * To change this template use File | Settings | File Templates.
+ */
+class Line {
+
+}
